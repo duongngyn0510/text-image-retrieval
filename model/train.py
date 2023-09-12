fec687a32ecb8fc3e95385318a739a8b5fc7ce82
duongngyn0510 <duong05102002@gmail.com>
2023-09-12 14:47:05 +0700

    refactor

--- model/train.py
+++ model/train.py
@@ -1,19 +1,20 @@
 # Need training on Colab
+import warnings
+
 import clip
 import torch
 import torch.nn as nn
-import warnings
-from tqdm import tqdm
-from loguru import logger
 from config import Config
-from custom_dataset import FashionDataset, BalancedBatchSampler
-from utils import load_dataloader, convert_models_to_fp32
+from custom_dataset import BalancedBatchSampler, FashionDataset
+from loguru import logger
+from tqdm import tqdm
+from utils import load_dataloader
 
 warnings.filterwarnings("ignore")
 
 BATCH_SIZE = Config.BATCH_SIZE
 TRAIN_DATALOADER_PATH = Config.TRAIN_DATALOADER_PATH
-TEST_DATALOADER_PATH =  Config.TEST_DATALOADER_PATH
+TEST_DATALOADER_PATH = Config.TEST_DATALOADER_PATH
 TRAIN_EPOCHS = Config.TRAIN_EPOCHS
 PRETRAINED_PATH = Config.PRETRAINED_PATH
 
@@ -24,7 +25,21 @@ logger.info(f"Device: {device}")
 loaded_train_dataloader = load_dataloader(TRAIN_DATALOADER_PATH)
 loaded_test_dataloader = load_dataloader(TEST_DATALOADER_PATH)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-5, betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2)
+
+def convert_models_to_fp32(model):
+    """
+    Convert model's parameters to FP32 format for update parameters in optimizer step
+    Args:
+        model: torch.nn.Module
+    """
+    for p in model.parameters():
+        p.data = p.data.float()
+        p.grad.data = p.grad.data.float()
+
+
+optimizer = torch.optim.Adam(
+    model.parameters(), lr=1e-5, betas=(0.9, 0.98), eps=1e-6, weight_decay=0.2
+)
 loss_img = nn.CrossEntropyLoss()
 loss_txt = nn.CrossEntropyLoss()
 
@@ -44,7 +59,10 @@ for epoch in range(1, TRAIN_EPOCHS + 1):
         ground_truth = torch.arange(BATCH_SIZE).to(device)
 
         # Compute loss
-        total_loss = (loss_img(logits_per_image, ground_truth) + loss_txt(logits_per_text, ground_truth)) / 2
+        total_loss = (
+            loss_img(logits_per_image, ground_truth)
+            + loss_txt(logits_per_text, ground_truth)
+        ) / 2
 
         # Backward pass
         if device == "cpu":
@@ -58,7 +76,9 @@ for epoch in range(1, TRAIN_EPOCHS + 1):
             optimizer.step()
             clip.model.convert_weights(model)
 
-        pbar.set_description(f"Epoch {epoch}/{TRAIN_EPOCHS}, Loss: {total_loss.item():.4f}")
+        pbar.set_description(
+            f"Epoch {epoch}/{TRAIN_EPOCHS}, Loss: {total_loss.item():.4f}"
+        )
 
     test_loss = 0
     with torch.no_grad():
@@ -73,7 +93,10 @@ for epoch in range(1, TRAIN_EPOCHS + 1):
             ground_truth = torch.arange(BATCH_SIZE).to(device)
 
             # Compute loss
-            total_loss = (loss_img(logits_per_image, ground_truth) + loss_txt(logits_per_text, ground_truth)) / 2
+            total_loss = (
+                loss_img(logits_per_image, ground_truth)
+                + loss_txt(logits_per_text, ground_truth)
+            ) / 2
             test_loss += total_loss
 
     print(f"Epoch {epoch}, Train loss {train_loss / len(loaded_train_dataloader)}")
--- model/utils.py
+++ model/utils.py
@@ -1,11 +1,18 @@
-import pickle
-import os
-import load_dataset
 import json
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
-from torchvision.transforms import InterpolationMode
-from loguru import logger
+import os
+import pickle
+
+import load_dataset
 from config import Config
+from loguru import logger
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    InterpolationMode,
+    Normalize,
+    Resize,
+    ToTensor,
+)
 
 BICUBIC = InterpolationMode.BICUBIC
 INPUT_RESOLUTION = Config.INPUT_RESOLUTION
@@ -19,52 +26,79 @@ def _convert_image_to_rgb(image):
 def preprocess(n_px=INPUT_RESOLUTION):
     """
     A torchvision transform that converts a PIL image into a tensor that the returned model can take as its input
+
     Args:
         n_px : int, default=224
         Input resolution
+
     Returns:
         preprocess : Callable[[PIL.Image], torch.Tensor]
     """
-    return Compose([
-        Resize(n_px, interpolation=BICUBIC),
-        CenterCrop(n_px),
-        _convert_image_to_rgb,
-        ToTensor(),
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
-    ])
+    return Compose(
+        [
+            Resize(n_px, interpolation=BICUBIC),
+            CenterCrop(n_px),
+            _convert_image_to_rgb,
+            ToTensor(),
+            Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
 
 
 def load_dataloader(path):
-    with open(path, 'rb') as f:
+    """
+    Load dataloader from pkl file
+
+    Args:
+        path (str) : Directory to dataloader fkl file
+
+    Returns:
+        torch.utils.data.DataLoader
+    """
+    with open(path, "rb") as f:
         loaded_dataloader = pickle.load(f)
     logger.info(f"Loaded {path.split('/')[-1].split('_')[0]} dataloader successfully!")
     return loaded_dataloader
 
 
-def convert_models_to_fp32(model):
-    for p in model.parameters():
-        p.data = p.data.float()
-        p.grad.data = p.grad.data.float()
-
-
 def get_dataset():
-    os.environ['EXTRACT_DIR'] = EXTRACT_DIR
-    load_dataset.download_dataset(dataset_url="https://huggingface.co/datasets/glami/glami-1m/resolve/main/GLAMI-1M-dataset--test-only.zip")
-    df = load_dataset.get_dataframe('test')[['category_name', 'image_file']]
-    df = df.drop_duplicates(subset=['image_file'])[['category_name', 'image_file']].reset_index(drop=True)
+    """
+    Return small GLAMI-1M-dataset in DataFrame form
+
+    Returns:
+        pandas.DataFrame
+    """
+    os.environ["EXTRACT_DIR"] = EXTRACT_DIR
+    load_dataset.download_dataset(
+        dataset_url="https://huggingface.co/datasets/glami/glami-1m/resolve/main/GLAMI-1M-dataset--test-only.zip"
+    )
+    df = load_dataset.get_dataframe("test")[["category_name", "image_file"]]
+    df = df.drop_duplicates(subset=["image_file"])[
+        ["category_name", "image_file"]
+    ].reset_index(drop=True)
     return df
 
 
 def save_embedding(path, vectors_upsert):
+    """
+    Save embedding images and their other data in json file
+
+    Args:
+        path (str): directory to save json file
+        vectors_upsert (List[Dict]): vectors to upsert to Pinecone vector database
+    """
     if not os.path.isfile(path):
-        with open(path, 'w') as f:
+        with open(path, "w") as f:
             json.dump(vectors_upsert, f)
     else:
-        with open(path, 'r') as f:
-            values = json.load(f)  
+        with open(path, "r") as f:
+            values = json.load(f)
         values.extend(vectors_upsert)
 
-        with open(path, 'w') as f:
-                json.dump(values, f)
-    
+        with open(path, "w") as f:
+            json.dump(values, f)
+
     logger.info(f"Saved embedding successfully in {path}!")
